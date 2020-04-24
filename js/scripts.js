8ca87eea5f6eff56be7ab5e6991a18282aa71690
Tristen Everett <spock214@gmail.com>
2020-04-24 11:56:24 -0700

    create functions similar to the last one for replacing 2 with Boop! and 3 with I'm sorry, Dave I'm afraid I can't do that.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
-var checkOne = function(array){
-  for(var index = 0; index < array.length; index++){
-    for(var i = 0; i < array[index].length; i++){
-      if(array[index][i] === "1"){
+var checkOne = function(array) {
+  for (var index = 0; index < array.length; index++) {
+    for (var i = 0; i < array[index].length; i++) {
+      if (array[index][i] === "1") {
         array[index] = "Beep!";
+      };
+    };
+  };
+  return array;
+};
+
+var checkTwo = function(array) {
+  for (var index = 0; index < array.length; index++) {
+    for (var i = 0; i < array[index].length; i++) {
+      if (array[index][i] === "2") {
+        array[index] = "Boop!";
+      };
+    };
+  };
+  return array;
+};
+
+var checkThree = function(array){
+  for (var index = 0; index < array.length; index++) {
+    for (var i = 0; i < array[index].length; i++) {
+      if (array[index][i] === "3") {
+        array[index] = "I'm sorry, Dave. I'm afraid I can't do that.";
       };
     };
   };
