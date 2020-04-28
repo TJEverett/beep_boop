74cead9aa40cd16a6222815103d04c9a83137392
Tristen Everett <spock214@gmail.com>
2020-04-28 09:15:38 -0700

    create a function to output an array of numbers from 0 to the inputed number

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+var counting = function(number) {
+  var arrayNumbers = [];
+  for (var index = 0; index <= number; index++) {
+    arrayNumbers.push(index);
+  };
+  return arrayNumbers;
+};
+
 var checkOne = function(array) {
   for (var index = 0; index < array.length; index++) {
     for (var i = 0; i < array[index].length; i++) {
